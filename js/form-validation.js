717499f27828566d10cf308bf431303aa5063081
Philip Kontekas <61799881+pkontekas@users.noreply.github.com>
2020-05-13 23:40:30 +0300

    130520 update

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,4 +1,4 @@
-// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// Example starter JavaScript for disabling form submissions if there are invalid fields
 // (function () {
 //     'use strict'
   
@@ -30,4 +30,57 @@
     form.classList.add('was-validated');
   })
 
-  $('#myModal form :input').val("");
+ //$('#myModal form :input').val("");
+
+function delAssForm(){
+  jQuery("input").each(function(){
+     jQuery(this).val('');
+  });
+  document.getElementById("courseSelect").selectedIndex = -1;
+}
+
+function delCourseForm(){
+  jQuery("input").each(function(){
+     jQuery(this).val('');
+  });
+  document.getElementById("stream").selectedIndex = -1;
+  document.getElementById("type").selectedIndex = -1;
+}
+
+function delTrPerCoForm(){
+  document.getElementById("trainerSelect").selectedIndex = -1;
+  document.getElementById("courseSelect").selectedIndex = -1;
+}
+
+function delAssPerCoForm(){
+  document.getElementById("assSelect").selectedIndex = -1;
+  document.getElementById("courseSelect").selectedIndex = -1;
+}
+
+function delStPerCoForm(){
+  document.getElementById("studentSelect").selectedIndex = -1;
+  document.getElementById("courseSelect").selectedIndex = -1;
+}
+
+function deleteForm(){
+  jQuery("input").each(function(){
+     jQuery(this).val('');
+  });
+}
+
+// function alertOK(){
+//   return console.log("Successful Creation!");
+// }
+
+function submitted() {
+  if (confirm('Do you want to submit?')) {
+      yourformelement.submit();
+  } else {
+      return false;
+  }
+}
+
+$('#my-multiples').chosen({
+  //Here you can change the value of the maximum allowed options
+  min_selected_options: 1
+});
