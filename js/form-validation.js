4eee506c0506effe63aa01f5b3046899a34bb20e
Philip Kontekas <61799881+pkontekas@users.noreply.github.com>
2020-05-15 12:16:19 +0300

    150520
    better validation instructions, fixed togglebar for phones.

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,34 +1,32 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-// (function () {
-//     'use strict'
-  
-//     window.addEventListener('load', function () {
-//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//       var forms = document.getElementsByClassName('needs-validation')
-  
-//       // Loop over them and prevent submission
-//       Array.prototype.filter.call(forms, function (form) {
-//         form.addEventListener('submit', function (event) {
-//           if (form.checkValidity() === false) {
-//             event.preventDefault()
-//             event.stopPropagation()
-//           }
-//           form.classList.add('was-validated')
-//         }, false)
-//       })
-//     }, false)
-//   }())
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
-  var form = document.querySelector('.needs-validation');
+  // var form = document.querySelector('.needs-validation');
 
-  form.addEventListener('submit', function(event)
-  {
-    if (form.checkValidity() === false){
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    form.classList.add('was-validated');
-  })
+  // form.addEventListener('submit', function(event)
+  // {
+  //   if (form.checkValidity() === false){
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //   }
+  //   form.classList.add('was-validated');
+  // })
 
  //$('#myModal form :input').val("");
 
